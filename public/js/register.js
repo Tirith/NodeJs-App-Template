4fe5c0dd810524a91f229be115e52c5ebfec3567
Tomasz Boruc <boructomek@gmail.com>
2020-12-24 16:00:46 +0100

    some changes

--- public/js/register.js
+++ public/js/register.js
@@ -3,21 +3,32 @@ const nameInput = $('#fname');
 const lastnameInput = $('#lname');
 const passInput = $('#pass');
 const submitBtn = $('#submit-btn');
-const errorLabel = $('.errors')
+const errorLabel = $('.errors');
+
+const validEmailIcon = $('#validEmailIcon');
+const validNameIcon = $('#validNameIcon');
+const validLastNameIcon = $('#validLastNameIcon');
+const validPasswordIcon = $('#validPasswordIcon');
 
 
 
+validEmailIcon.css('display', 'none');
+validNameIcon.css('display', 'none');
+validLastNameIcon.css('display', 'none');
+validPasswordIcon.css('display', 'none');
 
 const checkEmail = () => {
     if (!validator.isEmail(emailInput.val())) {
         emailInput.removeClass('input-success')
         emailInput.addClass('input-error')
-        console.log(validator.isEmail(emailInput.val()))
+        validEmailIcon.css('display', 'none');
+
         return false
     } else {
         emailInput.removeClass('input-error')
         emailInput.addClass('input-success')
-        console.log(validator.isEmail(emailInput.val()))
+        validEmailIcon.css('display', '');
+
         return true
     }
 
@@ -27,20 +38,24 @@ const checkName = () => {
     if (!validator.isLength(nameInput.val(), { min: 2 })) {
         nameInput.removeClass('input-success')
         nameInput.addClass('input-error')
+        validNameIcon.css('display', 'none');
         return false
     } else {
         nameInput.addClass('input-success')
+        validNameIcon.css('display', '');
         return true
     }
 }
 
 const checkLastName = () => {
     if (!validator.isLength(lastnameInput.val(), { min: 2 })) {
-        lastnameInput.removeClass('input-success')
-        lastnameInput.addClass('input-error')
+        lastnameInput.removeClass('input-success');
+        lastnameInput.addClass('input-error');
+        validLastNameIcon.css('display', 'none');
         return false
     } else {
-        lastnameInput.addClass('input-success')
+        lastnameInput.addClass('input-success');
+        validLastNameIcon.css('display', '');
         return true
     }
 
@@ -49,12 +64,14 @@ const checkLastName = () => {
 const checkPassword = () => {
 
     if (!validator.isLength(passInput.val(), { min: 5 })) {
-        passInput.removeClass('input-success')
-        passInput.addClass('input-error')
+        passInput.removeClass('input-success');
+        passInput.addClass('input-error');
+        validPasswordIcon.css('display', 'none');
 
         return false
     } else {
-        passInput.addClass('input-success')
+        passInput.addClass('input-success');
+        validPasswordIcon.css('display', '');
         return true
     }
 
